b697837a7ba01a9329d01b80df8afe53326efceb
Irove04 <maresctor4@alu.edu.gva.es>
2025-11-27 09:06:32 +0000

    despues de 4.1 admin

--- blog-cris/src/Controller/PageController.php
+++ blog-cris/src/Controller/PageController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Security;
 
 final class PageController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('page/index.html.twig', []);
+        // Crear formulario de registro
+        $user = new User();
+        $registrationForm = $this->createForm(RegistrationFormType::class, $user);
+
+        return $this->render('page/index.html.twig', [
+            'registrationForm' => $registrationForm->createView(),
+        ]);
     }
 
     #[Route('/about', name: 'about')]
@@ -21,14 +31,28 @@ final class PageController extends AbstractController
     }
 
     #[Route('/single_post1', name: 'single_post1')]
-    public function single_post1(): Response
+    public function single_post1(Security $security): Response
     {
+        if (!$security->getUser()) {
+            return $this->redirect($this->generateUrl('index', [
+                'login_required' => 1,
+                'redirect_to' => 'single_post1'
+            ]));
+        }
+
         return $this->render('page/single_post1.html.twig');
     }
 
     #[Route('/single_post2', name: 'single_post2')]
-    public function single_post2(): Response
+    public function single_post2(Security $security): Response
     {
+        if (!$security->getUser()) {
+            return $this->redirect($this->generateUrl('index', [
+                'login_required' => 1,
+                'redirect_to' => 'single_post2'
+            ]));
+        }
+
         return $this->render('page/single_post2.html.twig');
     }
 
